fe16cbf0e334d8a42424a8fc2f6de7be57e0e09a
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-02-11 09:18:21 +0100

    :rotating_light: dispatch improve markFileError

--- dispatch-service/src/main/java/de/muenchen/oss/swim/dispatcher/application/usecase/helper/FileHandlingHelper.java
+++ dispatch-service/src/main/java/de/muenchen/oss/swim/dispatcher/application/usecase/helper/FileHandlingHelper.java
@@ -13,6 +13,9 @@ public class FileHandlingHelper {
     public static final String FILE_EXTENSION_PDF = "pdf";
     public static final String FILE_EXTENSION_CSV = "csv";
 
+    private static final String ILLEGAL_CHARS_PATTERN = "[^\\w .-]";
+    private static final int TAG_MAX_VALUE_LENGTH = 256;
+
     private final FileSystemOutPort fileSystemOutPort;
     private final SwimDispatcherProperties swimDispatcherProperties;
 
@@ -25,9 +28,10 @@ public class FileHandlingHelper {
      */
     public void markFileError(final File file, final String stateTagKey, final Exception e) {
         // escape illegal chars from message
-        final String escapedMessage = e.getMessage().replaceAll("[^\\w .-]", " ");
+        final String escapedMessage = e.getMessage().replaceAll(ILLEGAL_CHARS_PATTERN, " ");
         // shorten exception message for tag value max 256 chars
-        final String shortenedExceptionMessage = escapedMessage.length() > 256 ? escapedMessage.substring(0, 256) : escapedMessage;
+        final String shortenedExceptionMessage = escapedMessage.length() > TAG_MAX_VALUE_LENGTH ? escapedMessage.substring(0, TAG_MAX_VALUE_LENGTH)
+                : escapedMessage;
         fileSystemOutPort.tagFile(file.bucket(), file.path(), Map.of(
                 stateTagKey, swimDispatcherProperties.getErrorStateValue(),
                 swimDispatcherProperties.getErrorClassTagKey(), e.getClass().getName(),

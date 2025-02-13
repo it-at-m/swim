386d3bbc53b9fd6fa00b4b8158350bb5e1533989
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-02-13 08:34:14 +0100

    :building_construction: handler-lib fix Exception visibility

--- handler-core/src/main/java/de/muenchen/oss/swim/libs/handlercore/adapter/in/FileProcessingException.java
+++ handler-core/src/main/java/de/muenchen/oss/swim/libs/handlercore/adapter/in/FileProcessingException.java
@@ -1,8 +1,8 @@
 package de.muenchen.oss.swim.libs.handlercore.adapter.in;
 
 @SuppressWarnings("PMD.MissingSerialVersionUID")
-class FileProcessingException extends RuntimeException {
-    public FileProcessingException(final Throwable cause) {
+public class FileProcessingException extends RuntimeException {
+    protected FileProcessingException(final Throwable cause) {
         super(cause);
     }
 }
--- handler-core/src/main/java/de/muenchen/oss/swim/libs/handlercore/adapter/out/streaming/MessagingException.java
+++ handler-core/src/main/java/de/muenchen/oss/swim/libs/handlercore/adapter/out/streaming/MessagingException.java
@@ -1,8 +1,8 @@
 package de.muenchen.oss.swim.libs.handlercore.adapter.out.streaming;
 
 @SuppressWarnings("PMD.MissingSerialVersionUID")
-class MessagingException extends RuntimeException {
-    public MessagingException(final String message) {
+public class MessagingException extends RuntimeException {
+    protected MessagingException(final String message) {
         super(message);
     }
 }

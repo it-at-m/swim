d33cae9a96eb6bd22bbe6c4a553e8f9805c95b41
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-02-17 10:48:12 +0100

    :bug: handler-core add AutoConfiguration

---
+++ handler-core/src/main/java/de/muenchen/oss/swim/libs/handlercore/HandlerCoreAutoConfiguration.java
@@ -0,0 +1,9 @@
+package de.muenchen.oss.swim.libs.handlercore;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan
+public class HandlerCoreAutoConfiguration {
+}

e00fc831b6a3e853d42e9252a8f9ca1a0bb911d6
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-03-24 10:56:49 +0100

    :bug: dms refactor use case to nested properties fix properties setter

--- dms-service/src/main/java/de/muenchen/oss/swim/dms/domain/model/DmsRequestContext.java
+++ dms-service/src/main/java/de/muenchen/oss/swim/dms/domain/model/DmsRequestContext.java
@@ -1,22 +1,26 @@
 package de.muenchen.oss.swim.dms.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Context for an DMS request.
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DmsRequestContext {
     /**
      * User under which a DMS request is executed.
      */
-    final String username;
+    private String username;
     /**
      * Used to resolve user role under which the DMS request is executed, default role if not defined.
      */
-    final String joboe;
+    private String joboe;
     /**
      * Used to resolve user role under which the DMS request is executed, default role if not defined.
      */
-    final String jobposition;
+    private String jobposition;
 }
--- dms-service/src/main/java/de/muenchen/oss/swim/dms/domain/model/DmsTarget.java
+++ dms-service/src/main/java/de/muenchen/oss/swim/dms/domain/model/DmsTarget.java
@@ -3,21 +3,23 @@ package de.muenchen.oss.swim.dms.domain.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.Objects;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 /**
  * Target for an DMS action.
  */
-@Getter
+@Data
+@NoArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class DmsTarget extends DmsRequestContext {
     /**
      * COO of an object.
      */
-    private final String coo;
+    private String coo;
 
     public DmsTarget(final String coo, @NotBlank final String username, final String joboe, final String jobposition) {
         super(username, joboe, jobposition);

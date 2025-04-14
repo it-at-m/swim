5d995a145eab6c7e95a985e7d22c0b120f91426d
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-04-14 08:01:51 +0200

    :memo: matching add java doc

--- matching-service/src/main/java/de/muenchen/oss/swim/matching/adapter/in/schedule/ScheduleAdapter.java
+++ matching-service/src/main/java/de/muenchen/oss/swim/matching/adapter/in/schedule/ScheduleAdapter.java
@@ -9,12 +9,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+/**
+ * Adapter for triggering scheduled operations (e.g. importing from DMS).
+ */
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ScheduleAdapter {
     private final ProcessDmsExportInPort processDmsExportInPort;
 
+    /**
+     * Trigger the import of matching data from the DMS via cron.
+     */
     @Scheduled(cron = "${swim.schedule-cron}")
     public void triggerProcessingViaDms() {
         try {

289f37daecaecfb1da9bfd75c9c332e32da3aaac
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-04-16 16:06:12 +0200

    :fire: dms rm response size workaround

--- dms-service/src/main/java/de/muenchen/oss/swim/dms/adapter/out/dms/ApiClientConfiguration.java
+++ dms-service/src/main/java/de/muenchen/oss/swim/dms/adapter/out/dms/ApiClientConfiguration.java
@@ -14,14 +14,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 class ApiClientConfiguration {
-    // FIXME remove workaround after API response was changed
-    private static final int MAX_RESPONSE_BODY_SIZE = 100 * 1024 * 1024;
-
     @Bean
     protected ApiClient apiClient(final DmsProperties dmsProperties) {
-        final WebClient webClient = WebClient.builder()
-                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(MAX_RESPONSE_BODY_SIZE))
-                .build();
+        final WebClient webClient = WebClient.builder().build();
         final ApiClient apiClient = new ApiClient(webClient);
         apiClient.setBasePath(dmsProperties.getBaseUrl());
         apiClient.setUsername(dmsProperties.getUsername());

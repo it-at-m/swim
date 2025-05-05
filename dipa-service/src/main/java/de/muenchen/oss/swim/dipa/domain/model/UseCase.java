4b2b9f5af1d5b70e760538275750c5ebafb2e05a
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-05-05 16:48:55 +0200

    fix(dipa): UseCase fix validation and defaults

--- dipa-service/src/main/java/de/muenchen/oss/swim/dipa/domain/model/UseCase.java
+++ dipa-service/src/main/java/de/muenchen/oss/swim/dipa/domain/model/UseCase.java
@@ -1,10 +1,13 @@
 package de.muenchen.oss.swim.dipa.domain.model;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
 @Data
+@Validated
 public class UseCase {
     @NotBlank
     private String name;
@@ -18,22 +21,26 @@ public class UseCase {
      * Defines how the target coo is gathered.
      */
     @NotNull
+    @Valid
     private UseCaseSource cooSource;
     /**
      * Properties for creating an Incoming.
      * Applies to {@link UseCaseType#getType()} being {@link DipaResourceType#INCOMING}.
      */
     @NotNull
+    @Valid
     private UseCaseIncoming incoming = new UseCaseIncoming();
     /**
      * Properties for creating a ContentObject.
      * Applies to {@link UseCaseType#getType()} being {@link DipaResourceType#INCOMING}.
      */
     @NotNull
+    @Valid
     private UseCaseContentObject contentObject = new UseCaseContentObject();
     /**
      * Context under which DiPa requests are made.
      */
     @NotNull
-    private DipaRequestContext context = new DipaRequestContext(null);
+    @Valid
+    private DipaRequestContext context;
 }

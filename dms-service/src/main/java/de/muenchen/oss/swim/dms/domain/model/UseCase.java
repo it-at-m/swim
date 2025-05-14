7b1e9aa06364e89441b4327fde1d5e09a56e6f85
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-05-14 07:42:53 +0200

    feat(dms): enable use case nested validation

--- dms-service/src/main/java/de/muenchen/oss/swim/dms/domain/model/UseCase.java
+++ dms-service/src/main/java/de/muenchen/oss/swim/dms/domain/model/UseCase.java
@@ -1,10 +1,13 @@
 package de.muenchen.oss.swim.dms.domain.model;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
 @Data
+@Validated
 public class UseCase {
     @NotBlank
     private String name;
@@ -17,23 +20,23 @@ public class UseCase {
     /**
      * Defines how the target coo is gathered.
      */
-    @NotNull
+    @NotNull @Valid
     private UseCaseSource cooSource;
     /**
      * Properties for creating an Incoming.
      * Applies to {@link UseCaseType#PROCEDURE_INCOMING}.
      */
-    @NotNull
+    @NotNull @Valid
     private UseCaseIncoming incoming = new UseCaseIncoming();
     /**
      * Properties for creating a ContentObject.
      * Applies to {@link UseCaseType#PROCEDURE_INCOMING} and {@link UseCaseType#PROCEDURE_INCOMING}.
      */
-    @NotNull
+    @NotNull @Valid
     private UseCaseContentObject contentObject = new UseCaseContentObject();
     /**
      * Context under which DMS requests are made.
      */
-    @NotNull
+    @NotNull @Valid
     private DmsRequestContext context = new DmsRequestContext(null, null, null);
 }
